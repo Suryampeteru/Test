38536f4f10c25c63a4eb07478417a0c569112410
suryam.peteru <suryam.peteru@gmail.com>
2022-09-16 13:26:15 +0530

    Add in Database

---
+++ Database.py
@@ -0,0 +1,4 @@
+Here data can store in the form of encryption format
+Database managed by cloud info
+The data can be access by user with there credentials
+Webserver and Appserver can to connect with in same network.
